712d566f23e4410bf6442f1bee219900e7a2a96f
Milenium666 <whiteangel64rus@gmail.com>
2023-03-13 07:25:13 +0300

    feat: filling the PanelRight component

--- src/components/PanelRight/PanelRight.js
+++ src/components/PanelRight/PanelRight.js
@@ -6,12 +6,41 @@ import WorkPlace from './WorkPlace/WorkPlace'
 
 import './PanelRight.css'
 
-function PanelRight() {
+function PanelRight(  {
+    items, boardId, board, dragOverHandler,
+    dragLeaveHandler, dragStartHandler, dragEndHandler,
+    dropHandler, dropCardHandler, handleClick, workplaceRef
+  }) {
     return(
         <section className="panelright">
             <CheckBox />
-            <Canvas />
-            <WorkPlace />
+        {
+          items.length === 0 ?
+          (
+            <Canvas
+              items={items}
+              board={board}
+              dragOverHandler={dragOverHandler}
+              dropCardHandler={dropCardHandler}
+            />
+          ) : (
+            <WorkPlace
+              items={items}
+              board={board}
+              dragOverHandler={dragOverHandler}
+              dragLeaveHandler={dragLeaveHandler}
+              dragStartHandler={dragStartHandler}
+              dragEndHandler={dragEndHandler}
+              dropHandler={dropHandler}
+
+              dropCardHandler={dropCardHandler}
+
+              handleClick={handleClick}
+
+              workplaceRef={workplaceRef}
+            />
+          )
+        }
         </section>
     )
 }
