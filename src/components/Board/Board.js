df6ee07e42ce6f61543fa2be59fb007c1e4b9684
Milenium666 <whiteangel64rus@gmail.com>
2023-03-13 06:39:17 +0300

    feat: filling the Board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,11 +5,48 @@ import PanelRight from '../PanelRight/PanelRight';
 
 
 
-function Board() {
+function Board({
+    boardId, board, items, dragOverHandler, dragLeaveHandler, dragStartHandler,
+    dragEndHandler, dropHandler, dropCardHandler, handleClick, workplaceRef
+}) {
     return(
         <>
-            <PanelLeft />
-            <PanelRight/>
+            {
+        boardId === 'left' ?
+        (
+          <PanelLeft
+            items={items}
+            boardId={boardId}
+            board={board}
+            dragOverHandler={dragOverHandler}
+            dragLeaveHandler={dragLeaveHandler}
+            dragStartHandler={dragStartHandler}
+            dragEndHandler={dragEndHandler}
+            dropHandler={dropHandler}
+
+            dropCardHandler={dropCardHandler}
+
+            handleClick={handleClick}
+          />
+        ) : (
+          <PanelRight
+            items={items}
+            boardId={boardId}
+            board={board}
+            dragOverHandler={dragOverHandler}
+            dragLeaveHandler={dragLeaveHandler}
+            dragStartHandler={dragStartHandler}
+            dragEndHandler={dragEndHandler}
+            dropHandler={dropHandler}
+
+            dropCardHandler={dropCardHandler}
+
+            handleClick={handleClick}
+
+            workplaceRef={workplaceRef}
+          />
+        )
+      }
         </>
         
 
