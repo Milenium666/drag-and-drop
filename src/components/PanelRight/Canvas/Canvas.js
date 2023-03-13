f0eb82beb56abb735c3f9dfce3844851f9ecc55a
Milenium666 <whiteangel64rus@gmail.com>
2023-03-13 07:43:37 +0300

    feat: filling the Canvas component

--- src/components/PanelRight/Canvas/Canvas.js
+++ src/components/PanelRight/Canvas/Canvas.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-import './Canvas.css'
+import add from '../../../images/add.svg';
 
-function Canvas() {
-    return(
-        <>
-        </>
+import './Canvas.css';
+
+function Canvas({ board, dragOverHandler, dropCardHandler }) {
+    function cardEnterBoard(e) {
+        e.target.style.background = '#F0F9FF'
+    }
+
+    function cardLeaveBoard(e) {
+        e.target.style.background = '#FFFFFF'
+    }
+    return (
+        <section
+            className="canvas"
+            onDragOver={(e) => { dragOverHandler(e) }}
+            onDrop={(e) => { dropCardHandler(e, board) }}
+
+            onDragEnter={(e) => { cardEnterBoard(e) }}
+            onDragLeave={(e) => { cardLeaveBoard(e) }}
+        >
+            <img
+                className="canvas__img"
+                alt="Изображение, которое показывает назначение данного элемента"
+                src={add}
+            />
+            <p className="canvas__up">
+                Перетащите сюда
+            </p>
+            <p className="canvas__bottom">
+                любой элемент из левой панели
+            </p>
+        </section>
     )
 }
 
